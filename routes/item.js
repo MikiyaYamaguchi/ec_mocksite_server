e2eb31ec7d2988b04fac4728c457aefe45091721
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2025-12-14 22:37:40 +0900

    全ての商品を取得するAPIの実装と、商品を追加するAPIの調整

--- routes/item.js
+++ routes/item.js
@@ -3,13 +3,33 @@ const router = express.Router()
 
 const ItemModel = require("../models/item")
 
-router.post("/", (req, res) => {
-	console.log("reqの中身：", req.body);
-	ItemModel.create(req.body).then((createdItem) => {
-		res.json(createdItem)
-	}).catch((err) => {
-		res.render("error", {message: `サーバエラー：${err}`})
-	})
+//全ての商品を取得するAPI
+router.get("/", async (req, res) => {
+	try {
+		const allItems = await ItemModel.find()
+		res.status(200).json({
+			data: allItems
+		})
+	} catch(err) {
+		res.status(500).json({
+			message: "エラーが発生しました。",
+			error: err.message
+		})
+	}
+})
+
+//商品を追加するAPI
+router.post("/", async (req, res) => {
+	try {
+		console.log("reqの中身：", req.body);
+		const createdItem = await ItemModel.create(req.body)
+		res.status(201).json(createdItem)
+	} catch(err) {
+		res.status(500).json({
+			message: "エラーが発生しました。",
+			error: err.message
+		})
+	}
 })
 
 module.exports = router;
