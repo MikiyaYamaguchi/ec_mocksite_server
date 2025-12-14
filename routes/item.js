1257e7567a173666e1c54984f70fb146d8cafdc7
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2025-12-14 23:14:55 +0900

    商品を削除するAPIを実装

--- routes/item.js
+++ routes/item.js
@@ -107,4 +107,24 @@ router.put("/:id", async(req, res) => {
 	}
 })
 
+//商品を削除するAPI
+router.delete("/:id", async(req, res) => {
+	try {
+		const deleteItem = await ItemModel.findByIdAndDelete(req.params.id)
+		if(!deleteItem) {
+			return res.status(404).json({
+				message: "データが見つかりませんでした。"
+			})
+		}
+		res.status(200).json({
+			data: deleteItem
+		})
+	} catch(err) {
+		res.status(500).json({
+			message: "エラーが発生しました。",
+			error: err.message
+		})
+	}
+})
+
 module.exports = router;
