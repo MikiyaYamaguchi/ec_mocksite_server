9366155c2cb7d89d279dbe3a0436474baa291153
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2025-12-16 09:52:02 +0900

    全てのユーザーを取得するAPIを実装

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,21 @@ const jwt = require("jsonwebtoken")
 
 const UserModel = require("../models/user")
 
+//全てのユーザーを取得するAPI
+router.get("/", async(req, res) => {
+  try {
+    const allUsers = await UserModel.find()
+    res.status(200).json({
+      data: allUsers
+    })
+  } catch(err) {
+    res.status(500).json({
+      message: "エラーが発生しました。",
+      error: err.message
+    })
+  }
+})
+
 //ユーザーを登録するAPI
 router.post("/", async (req, res) => {
   try {
