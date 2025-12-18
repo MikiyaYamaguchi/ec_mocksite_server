dbcf55365b481edcd4edb8434a1e457bc0eea4b9
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2025-12-19 00:31:39 +0900

    /routes/user.js内のuseIdのcookie保存についても、httpOnlyをfalseに変更

--- routes/user.js
+++ routes/user.js
@@ -103,7 +103,7 @@ router.post("/login/", async (req, res) => {
     })
     //HttpOnly Cookie に保存
     res.cookie("userId", loginUser._id.toString(), {
-      httpOnly: true,
+      httpOnly: false,
       secure: false,
       sameSite: "lax",
       path: "/",
@@ -170,7 +170,7 @@ router.post("/refresh", async(req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000
     })
     res.cookie("userId", user._id.toString(), {
-      httpOnly: true,
+      httpOnly: false,
       secure: false,
       sameSite: "lax",
       path: "/",
